b49ed463a3cb97616df8d2d9334ce686b5afa07b
Himanshu <103843176+Hitanmaster@users.noreply.github.com>
2024-08-28 17:23:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,20 +12,20 @@ def is_enabled(value, default):
     else:
         return default
 
-UPDATE = environ.get("SUPPORT", "https://t.me/ultroid_official")
-SUPPORT = environ.get("SUPPORT", "https://t.me/ultroid0fficial_chat")
-OWNERID = int(environ.get("OWNERID" ,"2016390875"))
-YT = environ.get("YT", "https://youtube.com/@phdlust")
+UPDATE = environ.get("SUPPORT", "")
+SUPPORT = environ.get("SUPPORT", "")
+OWNERID = int(environ.get("OWNERID" ,"7233389466"))
+YT = environ.get("YT", "")
 
 # Bot Information
-API_ID = int(environ.get("API_ID", "23929647"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "29324866"))
+API_HASH = environ.get("API_HASH", "2d41c2224fd999a81f6c90ce68b0454a")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6926547228:AAGWVc-PuZu0ZSU-q5pOu1AJCzshUkg16p8")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/0db18dde98ea3d0d4df0a.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7233389466').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
-PORT = environ.get("PORT", "8080")
+PORT = environ.get("PORT", "8585")
 
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "cloneuxb")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
+DB_URI = environ.get("DB_URI", "mongodb+srv://Himanshu:Himanshu0987@cluster0.86lnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "uxbfile")
 
 # Auto Delete Information
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002163116037"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "publicearn.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "765562fc440b6ae6275da96cad3370270073634d") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/ultroid_official/18") # how to open link 
 
 # Website Info:
